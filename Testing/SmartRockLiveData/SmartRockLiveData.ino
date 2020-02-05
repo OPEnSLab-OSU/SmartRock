74d5809561e3e6eda3ed161527c4e82fe2980edf
Martin Escoto <escotom@oregonstate.edu>
2020-02-05 15:09:45 -0800

    Hooked up sensor pins to plot live data via serial plotter
    Data from pins A0 and A2 plot data to via serial plotter in Arduino IDE. Other pins will have to hooked up for other sensors.

--- Testing/SmartRockLiveData/SmartRockLiveData.ino
+++ Testing/SmartRockLiveData/SmartRockLiveData.ino
@@ -21,6 +21,12 @@ LoomFactory<
 
 LoomManager Loom{ &ModuleFactory };
 
+int sensorPin_1 = A0; // hook up first sensor
+int sensorPin_2 = A2; // hook up second sensor
+
+int sensorValue_1 = 0; // initialize sensor 1 value to zero
+int sensorValue_2 = 0; // initialize sensor 2 value to zero
+
 void setup()
 {
     pinMode(5, OUTPUT);
@@ -29,7 +35,7 @@ void setup()
     digitalWrite(5, LOW);
     digitalWrite(6, HIGH);
 
-  Loom.begin_LED();
+    Loom.begin_LED();
 	Loom.flash_LED(10, 200, 200, true);
 	Loom.begin_serial();
 
@@ -46,6 +52,14 @@ void loop()
     Loom.measure();
     Loom.package();
     Loom.SDCARD().log();
+
+    sensorValue_1 = analogRead(sensorPin_1); // store sensor 1 pin value 
+    sensorValue_2 = analogRead(sensorPin_2); // store sensor 2 pin value
+
+    Serial.println(sensorValue_1); // plot sensor 1 value via serial plotter
+    Serial.print(","); // seperator; essential to have seperate graphed lines in serial plotter 
+    Serial.println(sensorValue_2); // plot sensor 2 value via serial plotter
+    delay(20); // wait 20ms so that data is spaced out just enough to be readable
 
     Loom.pause(1000);
 }
