b16406e3bf7ed9e8cd2e9073b3c9967ce2e6628f
colinhalebrown <colinhalebrown+GIT@gmail.com>
2021-01-07 13:40:54 -0800

    Update Code Library

---
+++ Code/SmartRockV3_Board_Test/config.h
@@ -0,0 +1,17 @@
+"{\
+	'general':\
+	{\
+		'name':'Device',\
+		'instance':1\
+	},\
+	'components':[\
+		{\
+			'name':'ADS1115',\
+			'params':[72, true, true, false, true, false, false, 0]\
+		},\
+   {\
+      'name':'MS5803',\
+      'params':[119]\
+    }\
+	]\
+}"
